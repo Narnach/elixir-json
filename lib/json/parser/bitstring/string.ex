ca4b70f817906d9aa155abb6945d9b984c8d8ff7
Carlos Brito Lage <carlos@carloslage.net>
2017-05-23 10:16:05 +0100

    experimentation

--- lib/json/parser/bitstring/string.ex
+++ lib/json/parser/bitstring/string.ex
@@ -1,5 +1,4 @@
 defmodule JSON.Parser.Bitstring.String do
-
   use Bitwise
   @doc """
   parses a valid JSON string, returns its elixir representation
@@ -36,47 +35,44 @@ defmodule JSON.Parser.Bitstring.String do
       iex> JSON.Parser.Bitstring.String.parse "\\\"Éloise woot\\\" Éloise"
       {:ok, "Éloise woot", " Éloise" }
   """
-  def parse(<< ?" :: utf8 , json :: binary >>), do: parse_string_contents(json, [ ])
-  def parse(<< >>), do: { :error, :unexpected_end_of_buffer }
-  def parse(json), do: { :error, { :unexpected_token, json } }
-
-
-  #stop conditions
-  defp parse_string_contents(<< >>, _), do: { :error, :unexpected_end_of_buffer }
-
-  # found the closing ", lets reverse the acc and encode it as a string!
-  defp parse_string_contents(<< ?" :: utf8, json :: binary >>, acc) do
-    case Enum.reverse(acc) |> List.to_string do
-      encoded when is_binary(encoded) ->
-        { :ok, encoded, json }
-      _ ->
-        {:error, { :unexpected_token, json }}
+  def parse(json) do
+    case json do
+      << ?" :: utf8 , rest :: binary >> -> parse_string_contents(rest, [])
+      << >> -> { :error, :unexpected_end_of_buffer }
+      _ -> { :error, { :unexpected_token, json } }
     end
   end
 
-  #parsing
-  defp parse_string_contents(<< ?\\, ?f,  json :: binary >>, acc), do: parse_string_contents(json, [ ?\f | acc ])
-  defp parse_string_contents(<< ?\\, ?n,  json :: binary >>, acc), do: parse_string_contents(json, [ ?\n | acc ])
-  defp parse_string_contents(<< ?\\, ?r,  json :: binary >>, acc), do: parse_string_contents(json, [ ?\r | acc ])
-  defp parse_string_contents(<< ?\\, ?t,  json :: binary >>, acc), do: parse_string_contents(json, [ ?\t | acc ])
-  defp parse_string_contents(<< ?\\, ?",  json :: binary >>, acc), do: parse_string_contents(json, [ ?"  | acc ])
-  defp parse_string_contents(<< ?\\, ?\\, json :: binary >>, acc), do: parse_string_contents(json, [ ?\\ | acc ])
-  defp parse_string_contents(<< ?\\, ?/,  json :: binary >>, acc), do: parse_string_contents(json, [ ?/  | acc ])
-
-  defp parse_string_contents(bin = << ?\\, ?u , _ :: binary >> , acc) do
-    case JSON.Parser.Bitstring.Unicode.parse(bin) do
-      { :error, error_info } -> { :error, error_info }
-      { :ok, decoded_unicode_codepoint, after_codepoint} ->
-        case decoded_unicode_codepoint do
-          << _ ::utf8 >> ->
-            parse_string_contents(after_codepoint, [ decoded_unicode_codepoint | acc ])
-          _ ->
-            { :error, { :unexpected_token, bin} }
+  defp parse_string_contents(json, acc) do
+    case json do
+      << >> -> { :error, :unexpected_end_of_buffer }
+      << ?\\, ?f,  rest :: binary >> -> parse_string_contents(rest, [ ?\f | acc ])
+      << ?\\, ?n,  rest :: binary >> -> parse_string_contents(rest, [ ?\n | acc ])
+      << ?\\, ?r,  rest :: binary >> -> parse_string_contents(rest, [ ?\r | acc ])
+      << ?\\, ?t,  rest :: binary >> -> parse_string_contents(rest, [ ?\t | acc ])
+      << ?\\, ?",  rest :: binary >> -> parse_string_contents(rest, [ ?"  | acc ])
+      << ?\\, ?\\, rest :: binary >> -> parse_string_contents(rest, [ ?\\ | acc ])
+      << ?\\, ?/,  rest :: binary >> -> parse_string_contents(rest, [ ?/  | acc ])
+      << ?\\, ?u , _ :: binary >> ->
+        case JSON.Parser.Bitstring.Unicode.parse(json) do
+          { :error, error_info } -> { :error, error_info }
+          { :ok, decoded_unicode_codepoint, after_codepoint} ->
+            case decoded_unicode_codepoint do
+              << _ :: utf8 >> ->
+                parse_string_contents(after_codepoint, [ decoded_unicode_codepoint | acc ])
+              _ ->
+                { :error, { :unexpected_token, json} }
+            end
         end
+      # found the closing ", lets reverse the acc and encode it as a string!
+      << ?" :: utf8, rest :: binary >> ->
+        case acc |> Enum.reverse |> List.to_string do
+          encoded when is_binary(encoded) ->
+            { :ok, encoded, rest }
+          _ ->
+            {:error, { :unexpected_token, json }}
+        end
+     << char :: utf8, rest :: binary >> -> parse_string_contents(rest, [ char | acc ])
     end
-  end
-
-  defp parse_string_contents(<< char :: utf8, json :: binary >>, acc) do
-    parse_string_contents(json, [ char | acc ])
   end
 end
